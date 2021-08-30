87d1c6466f9917d677745925686e1524c6254b0c
Yaroslav P <decss.j@gmail.com>
2021-08-31 02:56:11 +0400

    Add array range intersect merge

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Currently implemented:
 - Fibonacci (cycle and recursive)
 - Factorial (cycle and recursive)
 - Array items composition
+- Array range intersect merging
 - Clock hands Angle Calculator
---
+++ arrayIntersectMerge.php
@@ -0,0 +1,61 @@
+<?php 
+/**
+ * Search ranges like [4,7] and merge them if intersect
+ * Not optimized
+ * @param  array  $input    Array of range items [[1,5], [4,7], ...]
+ * @return array            Result array with non-intersected ranges
+ */
+function arrayIntersectMerge(array $input)
+{
+    usort($input, function($a, $b) {return $a[0] > $b[0];});
+    $result = [array_shift($input)];
+
+    foreach ($input as $value) {
+        $last = $result[count($result) - 1];
+
+        // In range
+        if ($value[0] >= $last[0] && $value[1] <= $last[1]) {
+            // skip
+
+        // Intersect from left
+        } else if (
+            ($value[0] < $last[0] && $value[1] >= $last[0] && $value[1] <= $last[1])
+            || ($value[1] + 1 == $last[0]) // next to the left 
+        ) {
+            $last[0] = $value[0];
+        
+        // Intersect from right
+        } else if (
+            ($value[0] >= $last[0] && $value[0] <= $last[1] && $value[1] > $last[1])
+            || ($value[0] - 1 == $last[1]) // next to the right
+        ) {
+            $last[1] = $value[1];
+            $result[count($result) - 1] = $last;
+
+        } else {
+            $result[] = $value;
+        }
+    }
+
+    return $result;
+}
+?>
+
+
+<b>Test</b>
+<pre>
+<?php
+$inputs = [
+    ['2-4', '7-9', '1-3', ],
+    ['7-10', '11-11', '15-15', '12-15', '44-49'],
+];
+foreach ($inputs as $input) {
+    $inputArray = [];
+    foreach ($input as $value) {
+        $inputArray[] = explode('-', $value);
+    }
+
+    echo 'For <b>[' . implode(', ', $input) . ']</b> ranges result is:<br>';
+    print_r(arrayIntersectMerge($inputArray));
+}
+?>
